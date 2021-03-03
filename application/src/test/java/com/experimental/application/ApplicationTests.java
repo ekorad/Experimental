e132a45eecc9e62bcfc4a5ac7158399d0d713f4f
Vlad Zahiu <vladzahiu28@gmail.com>
2021-03-03 18:11:54 +0200

    Initial commit
    Added blank Spring boot application

---
+++ application/src/test/java/com/experimental/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experimental.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
